8b81063fb6aa19089cc6760ff2614a20944f9103
Aleksander <interind@yandex.ru>
2021-07-20 14:54:48 +0300

    create styles

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -13,19 +13,27 @@ function Cards() {
         <button className="button button_type_filter" type="button" />
       </div>
       <ul className="container__cards">
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
-        <li className="container__card"><div className="container__product"/><span>1</span></li>
+        <li className="container__card">
+          <ul className="container__product container__product_type_order">
+            <li className="container__description container__description_type_info">
+              <h2 className="container__subtitle">
+                Заказ №123
+              </h2>
+              <span>
+                305
+              </span>
+            </li>
+            <li className="container__description container__description_type_time">
+              <span>
+                Сегодня: 12:00
+              </span>
+            </li>
+          </ul>
+          <ul className="container__product container__product_type_description">
+            <li className="container__description container__description_type_item">- товаров 5шт</li>
+            <li className="container__description container__description_type_product">- наименований 3шт</li>
+          </ul>
+        </li>
       </ul>
     </div>
   )
